20dc769ca3aa61e6ece1e385a7e5c593c9a32897
mrman888 <amiibospam@gmail.com>
2021-11-12 22:35:41 +0100

    added angular app

---
+++ bg-hoard/apps/store/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+    MatCardModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
