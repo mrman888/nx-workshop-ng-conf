48580bb93f133d03205d7d2d2bd985182c0e4516
mrman888 <amiibospam@gmail.com>
2021-11-12 23:21:35 +0100

    added toolbar

--- bg-hoard/apps/store/src/app/app.module.ts
+++ bg-hoard/apps/store/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    StoreUiSharedModule,
     BrowserModule,
     MatCardModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
